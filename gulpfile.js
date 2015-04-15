90ab44f5d13d7f0bff5ca2c64da16fce2c25ca23
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-15 23:48:41 +0200

    Update the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,51 @@
 'use strict';
 
-var gulp = require('gulp');
-var babel = require('gulp-babel');
 var del = require('del');
 
-// `gulp build`
-// -----------------------------------------------------------------------------
+var gulp = require('gulp-help')(require('gulp'));
+var babel = require('gulp-babel');
 
-gulp.task('build', ['clean', 'scripts']);
+// `gulp build`
+// ----------------------------------------------------------------------------
+gulp.task('build',
+  'Compile everything.',
+  ['clean', 'scripts']
+);
 
 // `gulp scripts`
-// -----------------------------------------------------------------------------
-
+// ----------------------------------------------------------------------------
 var scripts = {
   source: 'source/**/*.js'
 };
 
-gulp.task('scripts', ['scripts:es6', 'scripts:es5']);
+gulp.task('scripts',
+  'Build distribution scripts',
+  ['scripts:es6', 'scripts:es5']
+);
 
-gulp.task('scripts:es6',
+gulp.task('scripts:es6', false,
   ['scripts:clean'],
   function() {
     return gulp.src(scripts.source)
       .pipe(gulp.dest('dist.es6'))
-      ;
-  });
+    ;
+  }
+);
 
-gulp.task('scripts:es5',
+gulp.task('scripts:es5', false,
   ['scripts:clean'],
   function() {
     return gulp.src(scripts.source)
       .pipe(babel())
       .pipe(gulp.dest('dist.es5'))
-      ;
-  });
+    ;
+  }
+);
 
 gulp.task('scripts:clean', function(done) {
-  del(scripts.source, done);
+  del(['dist.es5', 'dist.es6'], done);
 });
 
 // `gulp clean`
-// -----------------------------------------------------------------------------
-
+// ----------------------------------------------------------------------------
 gulp.task('clean', ['scripts:clean']);
