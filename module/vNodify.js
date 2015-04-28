2fdfefa8a2de16886bbb8cf497b50d6d42f6bddc
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-28 15:37:10 +0200

    Get rid of hashifyAttributes

--- module/vNodify.js
+++ module/vNodify.js
@@ -9,7 +9,15 @@ let vNodeProto = {
 
 export default function vNodify(element) {
   let vNode = Object.create(vNodeProto);
-  vNode.properties = hashifyAttributes(element.attributes);
+
+  const properties = vNode.properties = {};
+  [].slice.call(element.attributes)
+    // NOTE: This should be `Array.from`, but there’s no support for that in node 0.12.
+    .forEach((attribute) => {
+      properties[attribute.name] = attribute.value;
+    })
+  ;
+
   if (element.tagName) vNode.tagName = element.tagName;
   return vNode;
 }
