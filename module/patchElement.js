43890b82c523eacb836f4b851263a46b5805eedb
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-29 20:12:57 +0200

    Fix patchElement

--- module/patchElement.js
+++ module/patchElement.js
@@ -1,5 +1,5 @@
 export default (element, patch) => {
-  const {firstPatch} = patch['0'];
+  const firstPatch = patch['0'].patch;
   for (let attribute in firstPatch) if (firstPatch.hasOwnProperty(attribute)) {
     if (firstPatch[attribute] == null) {
       element.removeAttribute(attribute);
