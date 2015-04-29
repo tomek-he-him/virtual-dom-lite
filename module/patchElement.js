52eabac8913ed369788f107cd286e90893b7e033
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-04-29 20:17:11 +0200

    Get rid of loops

--- module/patchElement.js
+++ module/patchElement.js
@@ -1,9 +1,7 @@
-export default (element, patch) => {
-  const firstPatch = patch['0'].patch;
-  for (let attribute in firstPatch) if (firstPatch.hasOwnProperty(attribute)) {
-    if (firstPatch[attribute] == null) {
-      element.removeAttribute(attribute);
-    }
-    else element.setAttribute(attribute, firstPatch[attribute]);
-  }
+export default (element, vPatch) => {
+  const {patch} = vPatch['0'];
+  Object.keys(patch).forEach((attribute) => {
+    if (patch[attribute] == null) element.removeAttribute(attribute);
+    else element.setAttribute(attribute, patch[attribute]);
+  });
 };
